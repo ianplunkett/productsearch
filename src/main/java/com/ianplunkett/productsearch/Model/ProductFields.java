edcbb8ae91bbcfa3ff9493524584c757c043082e
Ian Plunkett <ian.plunkett@gmail.com>
2020-01-06 23:12:52 -0800

    Static strings file

---
+++ src/main/java/com/ianplunkett/productsearch/Model/ProductFields.java
@@ -0,0 +1,26 @@
+package com.ianplunkett.productsearch.Model;
+
+public class ProductFields {
+
+    // Product Strings
+    public static final String PRODUCT_ID = "productId";
+    public static final String PRODUCT_DESCRIPTION = "productDescription";
+
+    // In Stock String
+    public static final String IN_STOCK = "inStock";
+
+    // Review Count Strings
+    public static final String REVIEW_COUNT = "reviewCount";
+    public static final String MAX_REVIEW_COUNT = "maxReviewCount";
+    public static final String MIN_REVIEW_COUNT = "minReviewCount";
+
+    // Price Strings
+    public static final String PRICE = "price";
+    public static final String MIN_PRICE = "minPrice";
+    public static final String MAX_PRICE = "maxPrice";
+
+    // Review Rating Strings
+    public static final String REVIEW_RATING = "reviewRating";
+    public static final String MIN_REVIEW_RATING = "minReviewRating";
+    public static final String MAX_REVIEW_RATING = "maxReviewRating";
+}
